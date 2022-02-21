0b6722aefae18a21e8b6231e80ab5a2f96cef677
Marc Cavazos <marcavazos19@icloud.com>
2022-02-21 12:03:09 -0500

    commit tree 7f6de004716d02d17cc4cc40dcf6dad64a3046b0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+    names.map do |name|
+        badge_maker(name)
+    end
+end
+
+def assign_rooms names
+    room = 0
+    names.map do |name|
+        room += 1
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer attendees
+    attendees.map do |attendee|
+        puts badge_maker(attendee)
+    end
+
+    assign_rooms(attendees).map do |message|
+        puts message
+    end
+end
